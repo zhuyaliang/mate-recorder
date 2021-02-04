2154f42df7e366f3ff1c75df5046fe2c9829383c
zhuyaliang <15132211195@163.com>
2021-02-04 15:01:45 +0800

    Delete unwanted message types

--- src/screen-server.c
+++ src/screen-server.c
@@ -137,38 +137,25 @@ static void cb_message (GstBus *bus, GstMessage *msg, gpointer data)
 {
     ScreenServer *ss = SCREEN_SERVER (data);
 
-  switch (GST_MESSAGE_TYPE (msg))
-  {
-    case GST_MESSAGE_ERROR: {
-      GError *err;
-      gchar *debug;
+    switch (GST_MESSAGE_TYPE (msg))
+    {
+        case GST_MESSAGE_ERROR:
+        {
+            GError *err;
+            gchar *debug;
 
-      gst_message_parse_error (msg, &err, &debug);
-      g_print ("Error: %s\n", err->message);
-      g_error_free (err);
-      g_free (debug);
-      gst_element_set_state (ss->priv->pipeline, GST_STATE_READY);
-      break;
-    }
-    case GST_MESSAGE_EOS:
-        gst_element_set_state (ss->priv->pipeline, GST_STATE_NULL);
-      break;
-    case GST_MESSAGE_BUFFERING: {
-      gint percent = 0;
-      gst_message_parse_buffering (msg, &percent);
-      g_print ("Buffering (%3d%%)\r", percent);
-      if (percent < 100)
-        gst_element_set_state (ss->priv->pipeline, GST_STATE_PAUSED);
-      else
-        gst_element_set_state (ss->priv->pipeline, GST_STATE_PLAYING);
-      break;
-    }
-    case GST_MESSAGE_CLOCK_LOST:
-      gst_element_set_state (ss->priv->pipeline, GST_STATE_PAUSED);
-      gst_element_set_state (ss->priv->pipeline, GST_STATE_PLAYING);
-      break;
-    default:
-      break;
+            gst_message_parse_error (msg, &err, &debug);
+            g_warning ("Error: %s\n", err->message);
+            g_error_free (err);
+            g_free (debug);
+            gst_element_set_state (ss->priv->pipeline, GST_STATE_NULL);
+            break;
+        }
+        case GST_MESSAGE_EOS:
+            gst_element_set_state (ss->priv->pipeline, GST_STATE_NULL);
+            break;
+        default:
+            break;
     }
 }
 
